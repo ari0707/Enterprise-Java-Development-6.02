684faf84c288d86a0ad9a4a2f14b02438f202004
Ariana7 <angela07rosales@gmail.com>
2023-01-07 14:22:06 +0100

    microservicios

---
+++ grades-data-service/src/main/java/com/ironhack/gradesdataservice/GradesDataServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ironhack.gradesdataservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradesDataServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradesDataServiceApplication.class, args);
+	}
+
+}
